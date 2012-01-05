1bc3a936a3675c3d2a3eb95894bc5c79d4827396
Regina Kim <eunyoung-kim@uiowa.edu>
2012-01-05 15:02:17 -0600

    DEV: RandomTree iterative Training

--- BRAINSCut/BRAINSCutTrainModel.cxx
+++ BRAINSCut/BRAINSCutTrainModel.cxx
@@ -233,7 +233,12 @@ BRAINSCutTrainModel
 /** random forest training */
 void
 BRAINSCutTrainModel
-::TrainRandomForest()
+::TrainRandomForest( int maxDepth, 
+                     int minSampleCount, 
+                     bool useSurrogates,
+                     bool calcVarImportance,
+                     int maxTreeCount
+                     )
 {
   CvRTrees forest;
 
